ca0958e5a076b05a9aed7acbe4fca29cc43426cc
Austin Sloane <sloane.ats@gmail.com>
2017-09-09 18:37:01 -0700

    Added movie list to the DOM using ES6

---
+++ app.jsx
@@ -0,0 +1,28 @@
+const movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'}
+];
+
+const App = () => (
+  <div class='app'>
+    <h1>Movie List</h1>
+    <MovieList movies={movies} />
+  </div>
+);
+
+const MovieList = (props) => (
+  <ul>
+    {props.movies.map( (movie, index) =>
+      <MovieListEntry key={index} movie={movie} />
+    )}
+  </ul>
+);
+
+const MovieListEntry = (props) => (
+  <li>{props.movie.title}</li>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
